2a1816e39c5b355758c7899b60c3345cbeb58d44
Addaian <85748975+Addaian@users.noreply.github.com>
2026-03-06 20:33:47 -0600

    added personal info page and settings

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,24 +1,13 @@
 export const dynamic = "force-dynamic";
 
-import { Sidebar } from "@/components/layout/sidebar";
-import { Topbar } from "@/components/layout/topbar";
+import { Navbar } from "@/components/layout/navbar";
 import { Toaster } from "@/components/ui/sonner";
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-screen overflow-hidden">
-      <div className="hidden md:flex">
-        <Sidebar />
-      </div>
-
-      <div className="flex flex-1 flex-col overflow-hidden">
-        <div className="md:hidden">
-          <Topbar />
-        </div>
-
-        <main className="flex-1 overflow-y-auto p-6">{children}</main>
-      </div>
-
+    <div className="flex h-screen flex-col overflow-hidden">
+      <Navbar />
+      <main className="flex-1 overflow-y-auto p-6">{children}</main>
       <Toaster />
     </div>
   );
